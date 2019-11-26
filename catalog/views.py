6eb2dce9430ce95b49236b3326b9495955a623e3
q4four <vlasov.ri@phystech.edu>
2019-11-27 02:05:07 +0300

    authorization

--- catalog/views.py
+++ catalog/views.py
@@ -7,12 +7,15 @@ def index(request):
     Функция отображения для домашней страницы сайта.
     """
     # Генерация "количеств" некоторых главных объектов
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
     # Доступные книги (статус = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # Метод 'all()' применен по умолчанию.
-    num_genres=Genre.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применен по умолчанию.
+    num_genres = Genre.objects.all().count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     # Отрисовка HTML-шаблона index.html с данными внутри 
     # переменной контекста context
     return render(
@@ -20,7 +23,7 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,
             'num_instances_available':num_instances_available,'num_authors':num_authors,
-            'num_genres':num_genres },
+            'num_genres':num_genres, 'num_visits':num_visits },
     )
 
 
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# After authorization
+LOGIN_REDIRECT_URL = '/catalog/'
+
+# Это регистрирует любые письма, отправленные на консоль
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
